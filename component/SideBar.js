96e4d8755f723ae164f8dc3f294c308619723f65
Md. Nayem Khan <nayemk900@gmail.com>
2022-02-27 23:43:16 +0600

    updated sidebar

--- component/SideBar.js
+++ component/SideBar.js
@@ -190,7 +190,7 @@ const SideBar = ({ setSidebar, handleOnOpen, handleClick }) => {
         <div
           className={`${
             open ? "translate-x-0" : "translate-x-full"
-          } lg:top-24 top-16 right-0 fixed bg-sidebar w-32 md:w-[18vw] lg:w-[12vw] h-4/5 z-40 ease-in-out duration-200 rounded-l-lg }`}
+          } md:top-24 top-0 bottom-0 right-0 fixed bg-sidebar w-32 md:w-[18vw] lg:w-[12vw] md:h-4/5 h-full z-40 ease-in-out duration-200 rounded-l-lg }`}
         >
           <div>
             <div className="mx-auto lg:px-10 lg:py-5 md:px-5 px-8 py-3 md:py-2">
@@ -200,7 +200,7 @@ const SideBar = ({ setSidebar, handleOnOpen, handleClick }) => {
                 className="w-full"
               />
             </div>
-            <div className="absolute lg:h-3/4 h-3/4 overflow-auto lg:pb-2 md:pb-6 w-11/12 mt-3">
+            <div className="absolute lg:h-4/5 h-4/5 overflow-auto lg:pb-2 md:pb-6 w-11/12 mt-1">
               <div>
                 <div className="mb-2">
                   <div className="bg-subTile lg:w-3/4 md:w-4/5 w-3/4 rounded-tr-lg mb-1 md:h-8">
@@ -215,9 +215,9 @@ const SideBar = ({ setSidebar, handleOnOpen, handleClick }) => {
                           <div
                             key={i}
                             onClick={()=> {handleClick, setActive(item.url)}}
-                            className={`${active === item.url ? 'border-white' : 'border-borderColor '} lg:border-4 border-4 rounded cursor-pointer`}
+                            className={`${active === item.url ? 'border-white' : 'border-borderColor '} h-14 lg:h-18 xl:h-20 border-4 xl:border-8 lg:border-4 rounded cursor-pointer`}
                           >
-                            <img src={item.url} alt="" className="w-full" />
+                            <img src={item.url} alt="" className="w-full h-full" />
                           </div>
                         </>
                       );
@@ -247,9 +247,9 @@ const SideBar = ({ setSidebar, handleOnOpen, handleClick }) => {
                         <div
                           key={i}
                           onClick={()=> {handleClick, setActive(item.url)}}
-                          className={`${active === item.url ? 'border-white' : 'border-borderColor '} lg:border-4 border-4 rounded cursor-pointer`}
+                          className={`${active === item.url ? 'border-white' : 'border-borderColor '} h-14 lg:h-18 xl:h-20 border-4 xl:border-8 lg:border-4 rounded cursor-pointer`}
                         >
-                          <img src={item.url} alt="" className="w-full" />
+                          <img src={item.url} alt="" className="w-full h-full" />
                         </div>
                       </>
                       );
@@ -269,9 +269,9 @@ const SideBar = ({ setSidebar, handleOnOpen, handleClick }) => {
                           <div
                             key={i}
                             onClick={()=> {handleClick, setActive(item.url)}}
-                            className={`${active === item.url ? 'border-white' : 'border-borderColor '} lg:border-4 border-4 rounded cursor-pointer`}
+                            className={`${active === item.url ? 'border-white' : 'border-borderColor '} h-14 lg:h-18 xl:h-20 border-4 xl:border-8 lg:border-4 rounded cursor-pointer`}
                           >
-                            <img src={item.url} alt="" className="w-full" />
+                            <img src={item.url} alt="" className="w-full h-full" />
                           </div>
                         </>
                       );
